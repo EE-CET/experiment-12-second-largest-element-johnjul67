71ea9416169a39e92c9c18ed4c096b3cdd2ef4bf
John <tve24el033@cet.ac.in>
2026-04-09 17:16:31 +0530

    c2

--- src/SecondLargest.java
+++ src/SecondLargest.java
@@ -1,33 +1,29 @@
 import java.util.Scanner;
 
-public class SecondLargest 
-{
-        static int max(int[] arr, int n)
-        {
-                if (n < 2)
-                        return -1;
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-                int m = -1, l = -1;
-                for (int i = 0; i < n; i++)
-                {
-                        if (m == -1 || arr[i] > arr[m])
-                        {
-                                l = m;
-                                m = i;
-                        }
-                        else if (arr[i] < arr[m] && (l == -1 || arr[i] > arr[l]))
-                                l = i;  
-                }
-                return l;
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
 
-        public static void main(String[] args)
-        {
-                Scanner sc=new Scanner(System.in);
-                int n=sc.nextInt();
-                int arr[]=new int[n];
-                for(int i=0;i<n;i++)
-                        arr[i]=sc.nextInt();
-                System.out.println(arr[max(arr, n)]);
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest) {
+                secondLargest = arr[i];
+            }
         }
+
+        System.out.println(secondLargest);
+        sc.close();
+    }
 }
